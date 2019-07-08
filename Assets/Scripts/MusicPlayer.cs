5b15b0330ad224df36926503df03bab90ad30195
kaustuv21 <prateeqkaustuv@gmail.com>
2019-07-08 19:57:53 +0530

    04 Added non-transient Music Player

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -5,13 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Invoke("LoadFirstScene",3f);
     }
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     void Update()
     {
 
