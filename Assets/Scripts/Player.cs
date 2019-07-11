6ec39ac44003256ee91af81b55ea61cd019f1dd9
kaustuv21 <prateeqkaustuv@gmail.com>
2019-07-11 23:27:17 +0530

    09 Added Vertical Movements

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,9 +6,9 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
 
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 25f;
+    [Tooltip("In ms^-1")][SerializeField] float speed = 25f;
     [Tooltip("In m")] [SerializeField] float xRange = 10f;
-
+    [Tooltip("In m")] [SerializeField] float yRange = 6f;
 
     void Start()
     {
@@ -19,15 +19,21 @@ public class Player : MonoBehaviour
     void Update()
     {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = xThrow * xSpeed;
+        float xOffset = xThrow * speed;
         float xOffsetThisFrame = xOffset * Time.deltaTime;
 
         float rawXPos = transform.localPosition.x + xOffsetThisFrame;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * speed;
+        float yOffsetThisFrame = yOffset * Time.deltaTime;
+
+        float rawYPos = transform.localPosition.y + yOffsetThisFrame;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
         transform.localPosition = new Vector3(clampedXPos,
-                                              transform.localPosition.y, 
+                                              clampedYPos, 
                                               transform.localPosition.z);
 
     }
