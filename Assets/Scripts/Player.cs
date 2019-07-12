4d8c61465c976ab733eabcc0cb47d960b180ab82
kaustuv21 <prateeqkaustuv@gmail.com>
2019-07-12 20:31:24 +0530

    10 Added LocalRotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,23 +18,36 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        processTranslation();
+        processRotation();
+
+    }
+
+    private void processTranslation()
+    {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
         float xOffset = xThrow * speed;
         float xOffsetThisFrame = xOffset * Time.deltaTime;
-
-        float rawXPos = transform.localPosition.x + xOffsetThisFrame;
-        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
-
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffset = yThrow * speed;
         float yOffsetThisFrame = yOffset * Time.deltaTime;
 
+        float rawXPos = transform.localPosition.x + xOffsetThisFrame;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
         float rawYPos = transform.localPosition.y + yOffsetThisFrame;
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
-        transform.localPosition = new Vector3(clampedXPos,
-                                              clampedYPos, 
-                                              transform.localPosition.z);
 
+        transform.localPosition = new Vector3(clampedXPos,
+                                              clampedYPos,
+                                              transform.localPosition.z);
+    }
+
+    private void processRotation()
+    {
+        transform.localRotation = Quaternion.Euler(transform.localRotation.x,
+                                                   transform.localRotation.y,
+                                                   transform.localRotation.z);
     }
 }
