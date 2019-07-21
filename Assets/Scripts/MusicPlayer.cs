5a0893d6cd7afd7d7d72599d48a0a800174f0f21
kaustuv21 <prateeqkaustuv@gmail.com>
2019-07-21 18:00:00 +0530

    016(+79) Added a standalone Script for Level change and tidied up assets

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -5,22 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
-    void Start()
-    {
-        Invoke("LoadFirstScene",3f);
-    }
-
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
-    }
-
-    void Update()
-    {
-
-    }
-    private void LoadFirstScene()
-    {
-        SceneManager.LoadScene(1);
     }
 }
