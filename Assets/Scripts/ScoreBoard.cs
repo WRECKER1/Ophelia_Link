b8c03d79efdd2152c7f07dc9645e987dda97e82f
kaustuv21 <prateeqkaustuv@gmail.com>
2019-07-24 19:39:48 +0530

    025 Added ScoreBoard Script

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreBoard : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] int scorePerHit = 15;
+
+    int score;
+    Text scoreText;
+
+
     void Start()
     {
-        
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ScoreHit()
     {
-        
+        score += scorePerHit;
     }
 }
